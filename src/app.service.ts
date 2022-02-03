e9113f8c5d61e36ddec1ca8ad22c10a1112c7437
Osagie Iyayi <osagieiyayi09@gmail.com>
2022-02-03 20:52:25 +0100

    edited the welcome rul

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Welcome to hornest wallet!, please go to this url https://documenter.getpostman.com/view/11690328/UVe9S9cc to view our documentation';
   }
 }
